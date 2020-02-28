770128cf5f381d5f885620de639167f0d56168a2
Momin Ahmad <mominahmad93@gmail.com>
2020-02-28 16:12:24 -0700

    more init stuff, added ui and basic function

--- popup.js
+++ popup.js
@@ -1,6 +1,14 @@
-let changeColor = document.getElementById('changeColor');
+// let changeColor = document.getElementById('changeColor');
 
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
+// chrome.storage.sync.get('color', function(data) {
+//   changeColor.style.backgroundColor = data.color;
+//   changeColor.setAttribute('value', data.color);
+// });
+let key = document.getElementById('key')
+let value = document.getElementById('value')
+
+function addValue(){
+    chrome.storage.sync.set({key:value}, function(){
+        console.log('Data added')
+    })
+}

723dc0fdd3d42e40c8aaa61aecdf219ddeee5639
Momin Ahmad <mominahmad93@gmail.com>
2020-02-28 18:41:14 -0700

    adding function buttons

--- popup.js
+++ popup.js
@@ -1,23 +1,7 @@
-function addValue(value){
-    let key = document.getElementById('key').value
-    console.log(key)
-    chrome.storage.sync.set({  [key] : value}, function(){
-        renderKeyValuePairs()
-    })
-}
+window.addEventListener('load', function(evt){
+    renderKeyValuePairs()
+})
 
-function renderKeyValuePairs(){
-    console.log('rendering')
-    chrome.storage.sync.get(null,function(data){
-        console.log(data)
-        var array = Object.values(data)
-        console.log(array)
-        var keyValuePair = document.createElement('div');
-    })
-
-    
-
-}
 
 document.getElementById('submit').addEventListener('click', function(event){
     event.preventDefault()
@@ -26,20 +10,72 @@ document.getElementById('submit').addEventListener('click', function(event){
     addValue(value)
 })
 
-// document.getElementById('show').addEventListener('click', function(event){
-//     event.preventDefault()
-   
-// })
 
 document.getElementById('clear').addEventListener('click', function(event){
     event.preventDefault()
     chrome.storage.sync.clear(function() {
+        const list = document.getElementById('keyValuePairs')
+        list.innerHTML = ''
         var error = chrome.runtime.lastError;
         if (error) {
             console.error(error);
         }
     });
 })
+
+document.addEventListene('click', function(event){
+    console.log(event)
+})
+
+
+function addValue(value){
+    let key = document.getElementById('key').value
+    console.log(key)
+    chrome.storage.sync.set({  [key] : value}, function(){
+        document.getElementById("form").reset();
+        renderKeyValuePairs()
+    })
+}
+
+
+function renderKeyValuePairs(){
+    console.log('rendering')
+    chrome.storage.sync.get(null,function(data){
+        console.log(data)
+        var valueArray = Object.values(data)
+        var keyArray = Object.keys(data)
+        console.log(valueArray)
+        console.log(keyArray)
+        for (i in keyArray){
+            var keyValuePair = document.createElement('div');
+
+            var keySpan = document.createElement('span')
+            var valueSpan = document.createElement('span')
+            var spaceSpan = document.createElement('span')
+
+            var copyText = document.createElement('button')
+            var deletePair = document.createElement('button')
+
+            var key = document.createTextNode(keyArray[i])
+            var value = document.createTextNode(valueArray[i])
+            var space = document.createTextNode(' : ')
+
+            keySpan.appendChild(key)
+            spaceSpan.appendChild(space)
+            valueSpan.appendChild(value)
+
+            keyValuePair.appendChild(keySpan)
+            keyValuePair.appendChild(spaceSpan)
+            keyValuePair.appendChild(valueSpan)
+            keyValuePair.appendChild(copyText)
+            keyValuePair.appendChild(deletePair)
+
+            keyValuePair.classList.add('keyPair')
+
+            document.getElementById('keyValuePairs').appendChild(keyValuePair)
+        }
+    })
+}
 
 
     

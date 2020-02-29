ee911b53b141720219efa23eca037cd6ba0f71bc
Momin Ahmad <mominahmad93@gmail.com>
2020-02-29 10:50:20 -0700

    basic functionality is done

--- popup.js
+++ popup.js
@@ -1,19 +1,18 @@
-window.addEventListener('load', function(evt){
+window.addEventListener('load', function (evt) {
     renderKeyValuePairs()
 })
 
 
-document.getElementById('submit').addEventListener('click', function(event){
+document.getElementById('submit').addEventListener('click', function (event) {
     event.preventDefault()
     let value = document.getElementById('value').value
-    console.log(value)
     addValue(value)
 })
 
 
-document.getElementById('clear').addEventListener('click', function(event){
+document.getElementById('clear').addEventListener('click', function (event) {
     event.preventDefault()
-    chrome.storage.sync.clear(function() {
+    chrome.storage.sync.clear(function () {
         const list = document.getElementById('keyValuePairs')
         list.innerHTML = ''
         var error = chrome.runtime.lastError;
@@ -24,37 +23,57 @@ document.getElementById('clear').addEventListener('click', function(event){
 })
 
 
+function renderKeyValuePairs() {
 
-function addValue(value){
-    let key = document.getElementById('key').value
-    console.log(key)
-    chrome.storage.sync.set({  [key] : value}, function(){
-        document.getElementById("form").reset();
-        renderKeyValuePairs()
-    })
-}
-
-
-function renderKeyValuePairs(){
-    console.log('rendering')
-    chrome.storage.sync.get(null,function(data){
+    chrome.storage.sync.get(null, function (data) {
         console.log(data)
         var valueArray = Object.values(data)
         var keyArray = Object.keys(data)
-        console.log(valueArray)
-        console.log(keyArray)
-        for (i in keyArray){
-            var keyValuePair = document.createElement('div');
+        var listIndex = 0;
 
+        for (x in valueArray) {
+            var keyValuePair = document.createElement('li');
+            keyValuePair.setAttribute('id', listIndex)
             var keySpan = document.createElement('span')
             var valueSpan = document.createElement('span')
             var spaceSpan = document.createElement('span')
 
             var copyText = document.createElement('button')
             var deletePair = document.createElement('button')
+            copyText.classList.add('itemButton')
+            deletePair.classList.add('itemButton')
+            copyText.innerHTML = 'copy'
+            deletePair.innerHTML = 'delete'
 
-            var key = document.createTextNode(keyArray[i])
-            var value = document.createTextNode(valueArray[i])
+            copyText.onclick = function (evt) {
+                var textToCopy = evt.path[1].childNodes[2].innerHTML
+                var textArea = document.createElement("textarea");
+                textArea.value = textToCopy
+                document.body.appendChild(textArea);
+                textArea.select();
+                document.execCommand("Copy");
+                textArea.remove();
+                alert(textArea.value + ' has been copied to clipboard')
+            }
+
+            deletePair.onclick = function (evt) {
+                var id = evt.path[1].id
+                var keyToDelete = evt.path[1].childNodes[0].innerHTML
+                console.log(keyToDelete)
+                var itemToDelete = document.getElementById(id)
+                console.log(itemToDelete)
+                document.getElementById('keyValuePairs').removeChild(itemToDelete)
+                chrome.storage.sync.remove(keyToDelete, function (data) {
+                    var error = chrome.runtime.lastError;
+                    if (error) {
+                        console.error(error);
+                    }
+                    console.log(data)
+                })
+            }
+
+            var key = document.createTextNode(keyArray[x])
+            var value = document.createTextNode(valueArray[x])
             var space = document.createTextNode(' : ')
 
             keySpan.appendChild(key)
@@ -67,12 +86,21 @@ function renderKeyValuePairs(){
             keyValuePair.appendChild(copyText)
             keyValuePair.appendChild(deletePair)
 
-            keyValuePair.classList.add('keyPair')
-
             document.getElementById('keyValuePairs').appendChild(keyValuePair)
+            listIndex++
         }
     })
 }
 
 
-    
+function addValue(value) {
+    let key = document.getElementById('key').value
+    var div = document.querySelector('ul')
+    div.innerHTML = ''
+    chrome.storage.sync.set({ [key]: value }, function () {
+        document.getElementById("form").reset();
+        renderKeyValuePairs()
+    })
+}
+
+

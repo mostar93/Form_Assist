ec8669d1ea284f7229d3728b92ec6200b4a1f101
Momin Ahmad <mominahmad93@gmail.com>
2020-02-28 17:33:17 -0700

    can store key value pairs and clear local storage

--- popup.js
+++ popup.js
@@ -1,14 +1,48 @@
-// let changeColor = document.getElementById('changeColor');
-
-// chrome.storage.sync.get('color', function(data) {
-//   changeColor.style.backgroundColor = data.color;
-//   changeColor.setAttribute('value', data.color);
-// });
-let key = document.getElementById('key')
-let value = document.getElementById('value')
-
-function addValue(){
-    chrome.storage.sync.set({key:value}, function(){
-        console.log('Data added')
+function addValue(value){
+    let key = document.getElementById('key').value
+    console.log(key)
+    chrome.storage.sync.set({  [key] : value}, function(){
+        renderKeyValuePairs()
     })
 }
+
+function renderKeyValuePairs(){
+    console.log('rendering')
+    chrome.storage.sync.get(null,function(data){
+        console.log(data)
+        var array = Object.values(data)
+        console.log(array)
+        var keyValuePair = document.createElement('div');
+        array.forEach(element => {
+            console.log(element)
+        });
+    })
+
+    
+
+}
+
+document.getElementById('submit').addEventListener('click', function(event){
+    event.preventDefault()
+    let value = document.getElementById('value').value
+    console.log(value)
+    addValue(value)
+})
+
+// document.getElementById('show').addEventListener('click', function(event){
+//     event.preventDefault()
+   
+// })
+
+document.getElementById('clear').addEventListener('click', function(event){
+    event.preventDefault()
+    chrome.storage.sync.clear(function() {
+        var error = chrome.runtime.lastError;
+        if (error) {
+            console.error(error);
+        }
+    });
+})
+
+
+    
